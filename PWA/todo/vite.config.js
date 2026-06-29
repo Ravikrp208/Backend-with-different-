617dc02d7bb6716183241b1b93ce9798a1438166
Ravikrp208 <ravikrp208@gmail.com>
2026-06-29 23:55:43 +0530

    PWA Learning

---
+++ PWA/todo/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA
+    ({registerType: 'autoUpdate',
+      manifest: {
+        name: 'Todo App',
+        short_name: 'Todo',
+        description: 'A simple todo app built with React and Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ] 
+})
