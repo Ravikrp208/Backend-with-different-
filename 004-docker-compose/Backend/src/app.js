568badd863f2b3e60624cb1d5ec0752a29287d17
Ravikrp208 <ravikrp208@gmail.com>
2026-07-05 23:28:22 +0530

    docker compose

--- 004-docker-compose/Backend/src/app.js
+++ 004-docker-compose/Backend/src/app.js
@@ -1,22 +1,41 @@
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 
-dotenv.config();
+const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello Ravi Baby");
+  res.send("Hello Ravi");
 });
 
 app.get("/home", (req, res) => {
-  res.send("This is home page");
+  res.send("Welcome to Home Page");
 });
 
-app.get("/about",(req,res)=>{
-    res.send("This is about page")
-})
+app.get("/about", (req, res) => {
+  res.send("Welcome to About Page");
+});
+
+
+app.get("/api/data",(req, res) => {
+  const data = {
+    message: "Hello from the backend!",
+    timestamp: new Date(),
+  };
+  res.json(data);
+ 
+});
+
+app.get("/api/users", (req, res) => {
+  const users = [
+    { id: 1, name: "Ravi Kumar Pandit" },
+    { id: 2, name: "Rani kumari" },
+    { id: 3, name: "Shivam dost" },
+  ];
+  res.json(users);
+});
 
 
 export default app;
