fca2db4feb75e45681f9d1e2f88e5f263c9a68df
Ravikrp208 <ravikrp208@gmail.com>
2026-07-08 23:34:53 +0530

    AWs docker compose

--- 005-docker-compose-Aws/Frontend/vite.config.js
+++ 005-docker-compose-Aws/Frontend/vite.config.js
@@ -5,9 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+    },
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },      
+      
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://backend:3000',
         changeOrigin: true,
         secure: false,
       },
