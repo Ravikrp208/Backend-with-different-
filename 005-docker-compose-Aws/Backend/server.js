9a6e50a0ebdad7f8566495468cf091902c3cee09
Ravikrp208 <ravikrp208@gmail.com>
2026-07-09 23:56:45 +0530

    Loas balancer

--- 005-docker-compose-Aws/Backend/server.js
+++ 005-docker-compose-Aws/Backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => 
 res.status(200).json({
@@ -32,6 +37,11 @@ app.get('/api/users', (req, res) => {
   ];
     res.json(users);
 });
+
+
+app.get("*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));     
+})
 
 
 app.listen(3000, () => {
