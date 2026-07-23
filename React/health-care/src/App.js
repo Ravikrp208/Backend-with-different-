19838261b07f7a6d44d929bd8dcb7474b5de2b6a
Ravikrp208 <ravikrp208@gmail.com>
2026-07-23 23:34:24 +0530

    Health care

---
+++ React/health-care/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Healthcare from './HealthCare';
+
+function App() {
+  return (
+    <div className="App">
+        <Healthcare/>
+    </div>
+  );
+}
+
+export default App;
